18ff6a5c6c5e6830d7bd6d09475279efb12d9fa4
李鹏 <kkkkkkkkkkkop.qq.com>
2017-05-03 15:24:32 +0800

    使用ButterKnife简化代码

--- app/src/main/java/cn/ucai/fulishe/ui/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulishe/ui/activity/SplashActivity.java
@@ -1,44 +1,38 @@
 package cn.ucai.fulishe.ui.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulishe.R;
 
 public class SplashActivity extends AppCompatActivity {
-    TextView tvSkip;
     private final static int time = 5000;
     MyCountDataTimer cdt;
+    @BindView(R.id.tvSkip)
+    TextView tvSkip;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skip);
-        initView();
+        ButterKnife.bind(this);
+        cdt = new MyCountDataTimer(time, 1000);
         cdt.start();
-        setListener();
     }
 
-    private void setListener() {
-        tvSkip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cdt.cancel();
-                cdt.onFinish();
-            }
-        });
+    @OnClick(R.id.tvSkip) void setTvSkip(){
+        cdt.cancel();
+        cdt.onFinish();
     }
 
-
-    private void initView() {
-        tvSkip = (TextView) findViewById(R.id.tvSkip);
-        cdt = new MyCountDataTimer(time,1000);
-    }
-
-    class MyCountDataTimer extends CountDownTimer{
+    class MyCountDataTimer extends CountDownTimer {
 
         /**
          * @param millisInFuture    The number of millis in the future from the call
@@ -53,12 +47,12 @@ public class SplashActivity extends AppCompatActivity {
 
         @Override
         public void onTick(long millisUntilFinished) {
-            tvSkip.setText(getString(R.string.skip)+" "+millisUntilFinished/1000+"s");
+            tvSkip.setText(getString(R.string.skip) + " " + millisUntilFinished / 1000 + "s");
         }
 
         @Override
         public void onFinish() {
-            startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
         }
     }
