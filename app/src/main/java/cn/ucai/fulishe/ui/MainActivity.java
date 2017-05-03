94aa15459a0a564c029f96da402d6b6995fd8479
李鹏 <kkkkkkkkkkkop.qq.com>
2017-05-03 14:07:46 +0800

    添加资源文件

--- app/src/main/java/cn/ucai/fulishe/ui/MainActivity.java
+++ app/src/main/java/cn/ucai/fulishe/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package cn.ucai.fulishe;
+package cn.ucai.fulishe.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cn.ucai.fulishe.R;
 
 public class MainActivity extends AppCompatActivity {
 
