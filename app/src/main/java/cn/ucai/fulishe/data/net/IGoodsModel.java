284ab40c602a46248cf3b1449a60632ddbcf671b
李鹏 <kkkkkkkkkkkop.qq.com>
2017-05-04 09:58:41 +0800

    新品数据的下载并在MainActivity中测试

---
+++ app/src/main/java/cn/ucai/fulishe/data/net/IGoodsModel.java
@@ -0,0 +1,14 @@
+package cn.ucai.fulishe.data.net;
+
+import android.content.Context;
+
+import cn.ucai.fulishe.data.bean.NewGoodsBean;
+import cn.ucai.fulishe.data.utils.OkHttpUtils;
+
+/**
+ * Created by Administrator on 2017/5/4.
+ */
+
+public interface IGoodsModel {
+    void loadNewGoodsData(Context context, int catId, int pageId, int pageSize, OkHttpUtils.OnCompleteListener<NewGoodsBean[]> listener);
+}
--- app/src/main/java/cn/ucai/fulishe/ui/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulishe/ui/activity/MainActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 
 import cn.ucai.fulishe.R;
+import cn.ucai.fulishe.data.bean.NewGoodsBean;
+import cn.ucai.fulishe.data.net.GoodsModel;
+import cn.ucai.fulishe.data.net.IGoodsModel;
+import cn.ucai.fulishe.data.utils.L;
+import cn.ucai.fulishe.data.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void onCheckedChange(View view){
+        testDownload();
+    }
 
+    private void testDownload() {
+        IGoodsModel model = new GoodsModel();
+        model.loadNewGoodsData(MainActivity.this, 0, 1, 10, new OkHttpUtils.OnCompleteListener<NewGoodsBean[]>() {
+            @Override
+            public void onSuccess(NewGoodsBean[] result) {
+                L.e("main","result="+result);
+                if(result!=null){
+                    L.e("main","result.length="+result.length);
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                L.e("mian","error"+error);
+            }
+        });
     }
 }
