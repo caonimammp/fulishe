439ff81e3d23da09257d7e7f4a66facee0b01484
李鹏 <kkkkkkkkkkkop.qq.com>
2017-05-09 14:57:57 +0800

    分类二级页面排序箭头的修改

--- app/src/main/java/cn/ucai/fulishe/ui/activity/Category_ChildActivity.java
+++ app/src/main/java/cn/ucai/fulishe/ui/activity/Category_ChildActivity.java
@@ -1,9 +1,12 @@
 package cn.ucai.fulishe.ui.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
@@ -13,9 +16,13 @@ import cn.ucai.fulishe.ui.fragment.GoodsFragment;
 
 public class Category_ChildActivity extends AppCompatActivity {
     GoodsFragment gf;
-    boolean priceAsc,addTimeAsc;
+    boolean priceAsc, addTimeAsc;
     int sortBy;
     Unbinder bind;
+    @BindView(R.id.tvPrice)
+    Button tvPrice;
+    @BindView(R.id.addTime)
+    Button addTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +38,34 @@ public class Category_ChildActivity extends AppCompatActivity {
 
     @OnClick({R.id.tvPrice, R.id.addTime})
     public void onViewClicked(View view) {
+        Drawable end;
         switch (view.getId()) {
             case R.id.tvPrice:
                 priceAsc = !priceAsc;
-                sortBy=priceAsc?I.SORT_BY_PRICE_DESC:I.SORT_BY_PRICE_ASC;
+                sortBy = priceAsc ? I.SORT_BY_PRICE_DESC : I.SORT_BY_PRICE_ASC;
+                end = getDrawable(priceAsc ? R.drawable.arrow_order_down : R.drawable.arrow_order_up);
+                tvPrice.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, end, null);
                 break;
             case R.id.addTime:
                 addTimeAsc = !addTimeAsc;
-                sortBy=addTimeAsc?I.SORT_BY_ADDTIME_DESC:I.SORT_BY_ADDTIME_ASC;
+                sortBy = addTimeAsc ? I.SORT_BY_ADDTIME_DESC : I.SORT_BY_ADDTIME_ASC;
+                end = getDrawable(addTimeAsc ? R.drawable.arrow_order_down : R.drawable.arrow_order_up);
+                addTime.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, end, null);
                 break;
         }
-            gf.sortGoods(sortBy);
+        gf.sortGoods(sortBy);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(bind!=null){
+        if (bind != null) {
             bind.unbind();
         }
+    }
+
+    @OnClick(R.id.ivBack)
+    public void onViewClicked() {
+        finish();
     }
 }
